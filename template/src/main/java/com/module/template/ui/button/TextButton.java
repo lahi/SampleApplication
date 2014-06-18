25479e7f826a4d15621c51abcd41a8cac8728da0
lahi <lahi@live.co.kr>
2014-06-18 19:17:53 +0900

    add button on module

---
+++ template/src/main/java/com/module/template/ui/button/TextButton.java
@@ -0,0 +1,23 @@
+package com.module.template.ui.button;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by sooyoungbyun on 2014. 6. 18..
+ */
+public class TextButton extends FrameLayout {
+
+    public TextButton(Context context) {
+        super(context);
+    }
+
+    public TextButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TextButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
